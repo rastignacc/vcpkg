04b3a29ac362eae274e5e2dca143ed2e6f630d9f
Rastislav Marko <rmarko@plentyoffish.com>
2022-07-05 04:51:36 -0700

    [gelflib] version update

--- ports/gelflib/portfile.cmake
+++ ports/gelflib/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rastignacc/gelflib
-    REF a9a5f927fc2190cf79aa732325dcedbdf193185b
-    SHA512 dfb0db6c2513c34f2945cead54a077cdf59cec17cace40d86ff0142771fb3e2e7aab6115f317a17c5e0b28aa8d6ecf2ed4e6f584beecccf3b8c439e54f2630ae
+    REF fe5dfa3243a4cafc9dd1df5c4fcf34ec19889ea5
+    SHA512 69c667cd4b3c80544cf95745b40c0033f18cb196b7ec508b7e46d2272215f446099efb0809878a67f5043eab7cdb4ad35ba170ca39b8d6f4534bb96f961cf132
     HEAD_REF master
 )
 
@@ -16,5 +16,3 @@ vcpkg_install_cmake()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/gelflib RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-file(COPY ${SOURCE_PATH}/src/GELFConfig.hpp DESTINATION ${CURRENT_INSTALLED_DIR}/include)
